08c3a598d1169671ffbe3d55eee2644b2095df53
Robert Christensen <robert.christensen@mymail.champlain.edu>
2020-02-01 16:39:41 -0500

    Updated Restrictor to use colliders. Added zone gravity.

--- Assets/ZoneGravity.cs
+++ Assets/ZoneGravity.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class ZoneGravity : MonoBehaviour
 {
+    [Tooltip("How does the gravity fall off?")]
+    public AnimationCurve falloffCurve = AnimationCurve.Linear(0, 1, 1, 0);
+
+    [Tooltip("How far should the maximum falloff distance be?")]
+    public float falloffMaxDist;
+
+    [Tooltip("Gravity to replace this with")]
     public Vector2 gravity;
 
-    void Update()
+    void OnTriggerStay2D(Collider2D other)
     {
-        
-    }
+        //Position of this object, in 2d
+        Vector2 pos2d = new Vector2(transform.position.x, transform.position.y);
 
-    void OnTriggerEnter2D()
-    {
+        Rigidbody2D rb;
+        if ((rb = other.gameObject.GetComponent<Rigidbody2D>()) != null)
+        {
+            //Calculate the normalized falloff
+            float falloffNrm = falloffCurve.Evaluate(
+                    (other.ClosestPoint(pos2d) - pos2d).magnitude * falloffMaxDist
+                );
 
+            //Replace gravity with what we want
+            rb.velocity += (gravity - (Vector2)Physics.gravity) * falloffNrm * Time.deltaTime;
+        }
     }
 }
--- Assets/ZoneRestrictor_old.cs
+++ Assets/ZoneRestrictor_old.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZoneRestrictor : MonoBehaviour
+public class ZoneRestrictor_old : MonoBehaviour
 {
-    [Tooltip("Collider (TRIGGER) to constrain this object to")]
-    public Collider2D restriction;
+    [Tooltip("RectTransform to constrain this object to")]
+    public RectTransform restriction;
 
     [Tooltip("How much relative acceleration is applied over time?")]
     public AnimationCurve accelerationCurve;
@@ -34,13 +34,17 @@ public class ZoneRestrictor : MonoBehaviour
         //Position of this object, in 2d
         Vector2 pos2d = new Vector2(transform.position.x, transform.position.y);
 
-        //Point closest to this object
-        Vector2 closest = restriction.ClosestPoint(pos2d);
+        //The minimum/maxmimum x/y of the constraining rectangle
+        //THIS IS A KLUDGE, FIX THIS LATER
+        float x1 = restriction.anchoredPosition.x - restriction.sizeDelta.x / 2;
+        float x2 = restriction.anchoredPosition.x + restriction.sizeDelta.x / 2;
+        float y1 = restriction.anchoredPosition.y - restriction.sizeDelta.y / 2;
+        float y2 = restriction.anchoredPosition.y + restriction.sizeDelta.y / 2;
 
-        //Apply the acceleration attempting to return the object to the collider
-        bool doReturnAccel = (pos2d - closest).magnitude > 0.1f;
+        //Apply the acceleration attempting to return the object to the bounding box?
+        bool doReturnAccel = pos2d.x < x1 || pos2d.x > x2 || pos2d.y < y1 || pos2d.y > y2;
 
-        //Track the amount of time spent accelerating towards the collider
+        //Track the amount of time spent accelerating towards the bounding box
         if (doReturnAccel)
         {
             timeSpentAccelerating += Time.deltaTime;
@@ -51,8 +55,14 @@ public class ZoneRestrictor : MonoBehaviour
             return;
         }
 
+        //Closest point within the Bounding Box
+        Vector2 targetPoint = new Vector2(
+            Mathf.Clamp(pos2d.x, x1, x2),
+            Mathf.Clamp(pos2d.y, y1, y2)
+        );
+
         //Accelerate in the direction of the closest point of the BB
-        Vector2 accel = (closest - pos2d);
+        Vector2 accel = (targetPoint - pos2d);
         //Set the minimum acceleration
         accel = accel.normalized * Mathf.Max(minAccel, accel.magnitude);
         //Accelerate according to the curve
