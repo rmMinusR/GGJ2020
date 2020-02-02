5d6c48fb3ccff3cac56081b569f529c92b6e3f9f
Robert Christensen <robert.christensen@mymail.champlain.edu>
2020-02-02 06:59:49 -0500

    Ship progress part 1

---
+++ Assets/Scenes/Level select/ProgressMonitor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressMonitor : MonoBehaviour
+{
+    public static bool[] progress;
+    private static bool hasBeenInitialized = false;
+
+    public GameObject[] objectivePieces;
+
+    public GameObject completionMessage;
+
+    private void Start()
+    {
+        if(!hasBeenInitialized)
+        {
+            progress = new bool[objectivePieces.Length];
+            for(int i = 0; i < objectivePieces.Length; i++)
+            {
+                progress[i] = false;
+            }
+            hasBeenInitialized = true;
+        }
+    }
+
+    private void Update()
+    {
+        bool isComplete = true;
+        for (int i = 0; i < objectivePieces.Length; i++)
+        {
+            objectivePieces[i].SetActive(progress[i]);
+            if (!progress[i]) isComplete = false;
+        }
+
+        completionMessage.SetActive(isComplete);
+    }
+}
