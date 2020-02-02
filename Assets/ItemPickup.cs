bf42d863ccd67c27350097e42be0c3c738db73d4
Robert Christensen <robert.christensen@mymail.champlain.edu>
2020-02-02 02:47:50 -0500

    Added Item Pickup

---
+++ Assets/ItemPickup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class ItemPickup : IObjective
+{
+    public GameObject renderObject;
+
+    public LevelStateManager levelStateManager;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (HasCompleted()) return;
+
+        if (other.gameObject.tag == "Player")
+        {
+            renderObject.SetActive(false);
+        }
+    }
+
+    public override bool HasCompleted()
+    {
+        return ! renderObject.activeInHierarchy;
+    }
+}
---
+++ Assets/LevelStateManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelStateManager : MonoBehaviour
+{
+    public List<IObjective> objectives;
+
+    public Object levelHub;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectives = new List<IObjective>(Object.FindObjectsOfType<IObjective>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool hasFinishedLevel = true;
+        foreach(IObjective o in objectives)
+        {
+            hasFinishedLevel = hasFinishedLevel && o.HasCompleted();
+        }
+
+        if(hasFinishedLevel)
+        {
+            SceneManager.LoadScene(levelHub.name);
+        }
+    }
+}
+
+public abstract class IObjective : MonoBehaviour
+{
+    public abstract bool HasCompleted();
+}
