f0b83833ebdf904ebcc35bcb0414343a2e00c678
Robert Christensen <robert.christensen@mymail.champlain.edu>
2020-02-02 13:02:52 -0500

    Added splash

---
+++ Assets/Scenes/Splash/DelayedGotoOnActivate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DelayedGotoOnActivate : MonoBehaviour
+{
+    public float delay = 1f;
+
+    public Object scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DelayedGoto());
+    }
+
+    IEnumerator DelayedGoto()
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(scene.name);
+    }
+}
