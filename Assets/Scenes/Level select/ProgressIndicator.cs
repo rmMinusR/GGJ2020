eceb2bc9e64b5400c090008efe21cbda832bbafb
Robert Christensen <robert.christensen@mymail.champlain.edu>
2020-02-02 11:39:46 -0500

    Change progress to checkboxes

---
+++ Assets/Scenes/Level select/ProgressIndicator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressIndicator : MonoBehaviour
+{
+    public SpriteRenderer target;
+
+    public Sprite sprEnabled, sprDisabled;
+
+    public void SetState(bool state)
+    {
+        target.sprite = state ? sprEnabled : sprDisabled;
+    }
+}
