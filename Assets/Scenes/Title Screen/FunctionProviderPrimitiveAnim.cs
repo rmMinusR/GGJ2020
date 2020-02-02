bef14b5223fa2ec6eba098b5c54935bcbd81890d
Robert Christensen <robert.christensen@mymail.champlain.edu>
2020-02-02 05:07:19 -0500

    Finished adding art assets. Patch skybox issues. Added intro animation.

---
+++ Assets/Scenes/Title Screen/FunctionProviderPrimitiveAnim.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class FunctionProviderPrimitiveAnim : MonoBehaviour
+{
+    private float frame = 0;
+    private bool isAnimating = false;
+
+    public float timePerFrame = 0.5f;
+
+    public Sprite[] sprites;
+
+    public Image target;
+
+    public UnityEvent whenDone;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isAnimating) return;
+
+        target.sprite = sprites[(int)frame];
+
+        frame += Time.deltaTime / timePerFrame;
+
+        if (frame >= sprites.Length)
+        {
+            if(whenDone != null) whenDone.Invoke();
+
+            Destroy(this);
+        }
+    }
+
+    public void StartAnimating()
+    {
+        isAnimating = true;
+    }
+}
