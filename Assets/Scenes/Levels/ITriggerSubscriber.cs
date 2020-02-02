08eabb7ad3da55b02710bbfe5078470d12d921a5
Robert Christensen <robert.christensen@mymail.champlain.edu>
2020-02-02 00:18:07 -0500

    More FS refactor. Added floating platform prefab.

--- Assets/Scenes/Levels/ITriggerSubscriber.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class ITriggerSubscriber : MonoBehaviour {
-    public ITrigger trigger;
-
-    public abstract void OnTriggerChange();
-}
