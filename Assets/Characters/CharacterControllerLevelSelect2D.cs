88cbfcdeafa894c1bebf71b8ea1f6c7ca5a3ad36
Robert Christensen <robert.christensen@mymail.champlain.edu>
2020-02-02 06:35:17 -0500

    Added basic level select screen with basic functionality

---
+++ Assets/Characters/CharacterControllerLevelSelect2D.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CharacterControllerLevelSelect2D : MonoBehaviour
+{
+    public string xAxis = "Horizontal";
+    public string yAxis = "Vertical";
+    public string jumpAxis = "Jump";
+
+    public float drag = 0.4f;
+    public float moveSpeed = 50f;
+
+    private void FixedUpdate()
+    {
+        // Read the inputs.
+        float dx = CrossPlatformInputManager.GetAxis(xAxis);
+        float dy = CrossPlatformInputManager.GetAxis(yAxis);
+
+        //Move
+        GetComponent<Rigidbody2D>().velocity *= drag;
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(dx, dy)*moveSpeed);
+
+        GetComponent<Animator>().SetFloat( "Speed", GetComponent<Rigidbody2D>().velocity.magnitude);
+
+        if(GetComponent<Rigidbody2D>().velocity.x > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else if(GetComponent<Rigidbody2D>().velocity.x < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+}
---
+++ Assets/Scenes/Level select/TimedZoneEventTrigger.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(LineRenderer))]
+public class TimedZoneEventTrigger : MonoBehaviour
+{
+    public UnityEvent triggeredEvent;
+
+    public float timeToTrigger = 3f;
+    public float timeToDeplete = 1f;
+
+    public float progress = 0;
+    public float radius = 10f;
+
+    void Update()
+    {
+        if(objectsInTrigger.Count > 0)
+        {
+            progress += Time.deltaTime / timeToTrigger;
+        } else
+        {
+            progress -= Time.deltaTime / timeToDeplete;
+        }
+        progress = Mathf.Clamp(progress, 0, 1);
+
+        Vector3[] arc = new Vector3[(int)(progress*50)];
+        for(int i = 0; i < arc.Length; i++)
+        {
+            arc[i] = (
+                    transform.right * Mathf.Cos(Mathf.PI * 2 * progress * ((float)i)/arc.Length) +
+                    transform.up    * Mathf.Sin(Mathf.PI * 2 * progress * ((float)i)/arc.Length)
+                ) * radius + transform.position;
+        }
+        GetComponent<LineRenderer>().positionCount = arc.Length;
+        GetComponent<LineRenderer>().SetPositions(arc);
+
+        if (progress >= 1) triggeredEvent.Invoke();
+    }
+
+
+    private List<GameObject> objectsInTrigger = new List<GameObject>();
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!objectsInTrigger.Contains(other.gameObject) && other.gameObject.tag == "Player") objectsInTrigger.Add(other.gameObject);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (objectsInTrigger.Contains(other.gameObject)) objectsInTrigger.Remove(other.gameObject);
+    }
+}
